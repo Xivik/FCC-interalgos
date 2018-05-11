1e0bb0ade243d7f28531e310905f01a4848bc995
Patrick Theijink <theijink@gmail.com>
2018-05-11 22:07:02 +0200

    smallest common multiple

--- interalgos.js
+++ interalgos.js
@@ -282,3 +282,30 @@ function sumPrimes(num) {
 }
 
 sumPrimes(977);
+
+// Smallest Common Multiple, Find the smallest common multiple of the provided parameters that 
+// can be evenly divided by both, as well as by all sequential numbers in the range between these parameters
+
+function smallestCommons(arr) {
+    // create empty array, push the range of arr onto range
+    let range = [];
+    for (let i = Math.max(...arr); i >= Math.min(...arr); i--) {
+    range.push(i);
+    } 
+    // Implement the Euclidean Algorithm
+    function gcd(x, y) {    
+      if (y === 0) {
+        return x;
+      } else {
+        return gcd(y, x % y);
+      }
+    } 
+    // loop through the range array and find the lowest common multiplier
+    let lowestCommon = range[0];
+    for (i = 1; i < range.length; i++) {
+      const GreatestCommonDivisor = gcd(lowestCommon, range[i]);
+      lowestCommon = (lowestCommon * range[i]) / GreatestCommonDivisor;
+    }
+    return lowestCommon;
+}
+smallestCommons([1,5]);
